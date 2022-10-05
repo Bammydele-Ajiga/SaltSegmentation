0aafaaea1f2f380662724acc1ef29431a976df75
Bammydele-Ajiga <104144120+Bammydele-Ajiga@users.noreply.github.com>
2022-10-05 12:47:26 +0100

    Add files via upload

---
+++ salt_segmentation (copy)/app.py
@@ -0,0 +1,58 @@
+
+from flask import Flask, render_template, request, redirect, flash
+from werkzeug.utils import secure_filename
+from main import get_pred
+import os
+from skimage.io import imread, imshow
+from PIL import Image
+import numpy as np
+
+#Save images to the 'static' folder as Flask serves images from this directory
+UPLOAD_FOLDER = 'static/images/'
+
+#Create an app object using the Flask class. 
+app = Flask(__name__, static_folder="static")
+
+
+app.secret_key = "secret key"
+
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#Add Post method to the decorator to allow for form submission. 
+@app.route('/', methods=['POST'])
+def submit_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)  #Use this werkzeug method to secure filename. 
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            #getPrediction(filename)
+            res = get_pred(filename)
+            # flash(res)
+            res = Image.fromarray((res* 255).astype(np.uint8))
+            basewidth = 300
+            wpercent = (basewidth/float(res.size[0]))
+            hsize = int((float(res.size[1])*float(wpercent)))
+            img = res.resize((basewidth,hsize), Image.ANTIALIAS)
+            img.save("static/images/result.png")
+            # plt.savefig()
+            # full_filename = os.path.join(app.config['UPLOAD_FOLDER'], "result.png")
+            full_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            flash(full_filename)
+            return redirect('/')
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ salt_segmentation (copy)/main.py
@@ -0,0 +1,28 @@
+
+#test_prediction =getPrediction('example.jpg')
+
+from keras.preprocessing.image import load_img, img_to_array
+from keras.models import Model, load_model
+from skimage.io import imread, imshow
+from skimage.transform import resize
+import numpy as np
+def get_pred(filepath):
+  img = load_img(filepath)
+  input = img_to_array(img)[:,:,1]
+  X_test = np.zeros((1, 64, 64, 1), dtype=np.uint8)
+  inputs = resize(input, (64, 64, 1), mode='constant', preserve_range=True)
+  X_test[0] = inputs
+  print(input.shape)
+  model = load_model('/home/bams/Downloads/salt _seg_model/segmentation')
+  result = model.predict(X_test, verbose=1)
+  result_t = (result > 0.5).astype(np.uint8)
+  tmp = np.squeeze(result).astype(np.float32)
+  res = np.dstack((tmp,tmp,tmp))
+  return res
+#   imshow(np.dstack((tmp,tmp,tmp)))
+#   plt.show()
+
+# res = get_pred("00a3af90ab.png")
+
+# imshow(res)
+# plt.savefig("result.png")
